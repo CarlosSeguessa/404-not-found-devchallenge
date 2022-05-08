4a953e6977db3345ecae4e44e5bba199e0111565
Carlos Seguessa <carlosseguessa@gmail.com>
2022-05-08 20:00:07 -0300

    404 not found page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inconsolata: "'Inconsolata', monospace",
+        montserrat: "'Montserrat', sans-serif",
+        spacemono: "'Space Mono', monospace",
+      },
+    },
+  },
+  plugins: [],
+}
